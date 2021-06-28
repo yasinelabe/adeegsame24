c1e44ce3834328ad710795bf2257464c89e4e540
= <=>
2021-06-28 12:42:20 +0300

    changed api

--- src/components/Presentation/Checkout.js
+++ src/components/Presentation/Checkout.js
@@ -15,53 +15,53 @@ export default function Checkout(props) {
       context.getCart(),
       { token: localStorage.getItem("token"), totalprice: totalprice },
     ];
-    // axios({
-    //   method: "post",
-    //   url: API_URL + "/create_order",
-    //   headers: { "content-type": "application/json" },
-    //   data: JSON.stringify(order),
-    // })
-    //   .then((res) => {
-    //     let data = res.data;
-    //     if (data.result !== false) {
-    //       window.location.href = "/success"
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-     axios({
+    axios({
       method: "post",
-      url: "https://api.waafi.com/asm",
+      url: API_URL + "/create_order",
       headers: { "content-type": "application/json" },
-      data: JSON.stringify({
-        "schemaVersion": "1.0",
-        "requestId": "123122",
-        "timestamp": Date.now(),
-        "channelName": "WEB",
-        "serviceName": "API_PURCHASE",
-        "sessionId": localStorage.getItem("token"),
-        "serviceParams": {
-            "merchantUid": "M0910238",
-            "apiUserId": "1000307",
-            "apiKey": "API-1441673838AHX",
-            "paymentMethod": "MWALLET_ACCOUNT",
-            "payerInfo": {
-              "accountNo": JSON.parse(localStorage.getItem("user"))[0].phone,
-              "accountHolder": JSON.parse(localStorage.getItem("user"))[0].fullname
-            },
-            
-            "transactionInfo": {
-              "referenceId": 'testing',
-                "invoiceId": "8789wwerer2342344",
-                "amount": totalprice / 8500,
-                "currency": "USD",
-                "description": "iib adeegsame24 "
-             
-            }
-        }
-    }),
+      data: JSON.stringify(order),
     })
+      .then((res) => {
+        let data = res.data;
+        if (data.result !== false) {
+          window.location.href = "/success"
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    //  axios({
+    //   method: "post",
+    //   url: "https://api.waafi.com/asm",
+    //   headers: { "content-type": "application/json" },
+    //   data: JSON.stringify({
+    //     "schemaVersion": "1.0",
+    //     "requestId": "123122",
+    //     "timestamp": Date.now(),
+    //     "channelName": "WEB",
+    //     "serviceName": "API_PURCHASE",
+    //     "sessionId": localStorage.getItem("token"),
+    //     "serviceParams": {
+    //         "merchantUid": "M0910238",
+    //         "apiUserId": "1000307",
+    //         "apiKey": "API-1441673838AHX",
+    //         "paymentMethod": "MWALLET_ACCOUNT",
+    //         "payerInfo": {
+    //           "accountNo": JSON.parse(localStorage.getItem("user"))[0].phone,
+    //           "accountHolder": JSON.parse(localStorage.getItem("user"))[0].fullname
+    //         },
+            
+    //         "transactionInfo": {
+    //           "referenceId": 'testing',
+    //             "invoiceId": "8789wwerer2342344",
+    //             "amount": totalprice / 8500,
+    //             "currency": "USD",
+    //             "description": "iib adeegsame24 "
+             
+    //         }
+    //     }
+    // }),
+    // })
       .then((res) => {
        console.log(res)
       })
